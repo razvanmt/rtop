c35b57590b62db4f0b2b94bcaf22636a01aea943
razvanmt <twostepsout@gmail.com>
2023-11-02 21:48:07 +0200

    added cpu usage

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use axum::{Router, Server, routing::get};
+use std::sync::{Arc, Mutex};
+
+use axum::{Router, Server, routing::get, extract::State};
+use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-    .route("/", get(root_get));
+    .route("/", get(root_get))
+    .with_state(AppState {sys: Arc::new(Mutex::new(System::new())) 
+    });
 
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
@@ -13,7 +18,22 @@ async fn main() {
     println!("Hello, world!");
 }
 
+#[derive(Clone)]
+struct AppState{
+    sys: Arc<Mutex<System>>,
+}
 
-async fn root_get() -> &'static str {
-    "Hi from Axum!"
+async fn root_get(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+    let mut s = String::new();
+
+    let mut sys = state.sys.lock().unwrap();
+        sys.refresh_cpu();
+    for (i, cpu) in sys.cpus().iter().enumerate(){
+        let i = i + 1;
+        let usage = cpu.cpu_usage();
+        writeln!(&mut s, "CPU {i} {usage}%").unwrap();
+    }
+
+    return s;
 }
